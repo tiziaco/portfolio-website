8a4ffdbefd92e1d89d0783efdafd05db65f3f0f6
tiziaco <tiziano.iaco@gmail.com>
2024-06-05 19:51:06 +0200

    modify models and admin

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -2,15 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Tag(models.Model):
-	name = models.CharField(max_length=100, unique=True)
+	name = models.CharField(max_length=50, unique=True)
 
 	def __str__(self):
 		return self.name
- 
+
+class Stack(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return self.name
+
 class Project(models.Model):
 	title = models.CharField(max_length=200)
+	card_img = models.OneToOneField('ProjectImage', on_delete=models.SET_NULL, blank=True, null=True, related_name='card_project')
 	description = models.TextField()
+	content = models.TextField(default="", blank=True, null=True)
 	tags = models.ManyToManyField(Tag, related_name='projects')
+	stack = models.ManyToManyField(Stack, related_name='projects', blank=True)
 	link = models.URLField(max_length=200, blank=True)
 
 	def __str__(self):
