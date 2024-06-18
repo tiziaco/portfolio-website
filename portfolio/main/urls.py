fb0a78d2970a87af76b84275b1c7c59b9fc9eb62
tiziaco <tiziano.iaco@gmail.com>
2024-06-18 22:45:52 +0200

    implement projrect page path

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	path("", views.home, name='home'),
 	path("home/", views.home, name='home'),
-	path("project/<int:id>/", views.project, name='project')
+	path('project/<str:project_title>/', views.project, name='project_detail')
 ]
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -4,7 +4,12 @@ from .models import Project, Tag
 def home(request):
 	projects = Project.objects.all()
 	tags = Tag.objects.all()
-	return render(request, 'home.html', {"projects": projects, "tags": tags})
+	user_about = request.user.about if request.user is not None else "Default about text or empty string"
+	return render(request, 'home.html', 
+			   {"projects": projects, 
+	   			"tags": tags,
+				"user_about": user_about})
 
-def project(request, id):
-	return render(request, 'project.html')
+def project(request, project_title):
+    project = get_object_or_404(Project, title=project_title)
+    return render(request, 'project.html', {'project': project})
