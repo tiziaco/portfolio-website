c58f50eaa9dcdff5eb1f0fc328728485a4b9364f
tiziaco <tiziano.iaco@gmail.com>
2024-06-18 12:45:36 +0200

    update html

--- portfolio/main/static/js/home.js
+++ portfolio/main/static/js/home.js
@@ -2,11 +2,20 @@ function filterByTag(selectedTag, $projects) {
 	$projects.each(function() {
 		const $project = $(this);
 		const projectTags = $project.data('tags');
+		const column = $project.closest('.col-md-4');
 
+		// console.log("	" + $project);
+		console.log("	" + projectTags);
+		console.log("	" + column)
+		console.log("	res: " + projectTags.includes(selectedTag))
 		if (selectedTag === 'all' || (projectTags && projectTags.includes(selectedTag))) {
-			$project.closest('.col-md-4').show()
+			column.css('display', 'flex');
+			console.log("	SHOW");
+			console.log($project.closest('.col-md-4'))
 		} else {
-			$project.closest('.col-md-4').hide();
+			column.css('display', 'none');
+			console.log("	HIDE");
+			console.log($project.closest('.col-md-4'))
 		}
 	});
 }
@@ -17,31 +26,32 @@ $(document).ready(function() {
 
 	$tags.on("click", function() {
 		const selectedTag = $(this).data('tag');
+		console.log("Filter button clicked: " + selectedTag);
 		filterByTag(selectedTag, $projects);
 	});
 });
 
-$(document).ready(function() {
-	const interBubble = $('.interactive');
-	let curX = 0;
-	let curY = 0;
-	let tgX = 0;
-	let tgY = 0;
+// $(document).ready(function() {
+// 	const interBubble = $('.interactive');
+// 	let curX = 0;
+// 	let curY = 0;
+// 	let tgX = 0;
+// 	let tgY = 0;
 
-	function move() {
-		curX += (tgX - curX) / 20;
-		curY += (tgY - curY) / 20;
-		interBubble.css('transform', `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`);
-		requestAnimationFrame(() => {
-			move();
-		});
-	}
+// 	function move() {
+// 		curX += (tgX - curX) / 20;
+// 		curY += (tgY - curY) / 20;
+// 		interBubble.css('transform', `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`);
+// 		requestAnimationFrame(() => {
+// 			move();
+// 		});
+// 	}
 
-	$(window).on('mousemove', function(event) {
-		tgX = event.clientX;
-		tgY = event.clientY;
-		// console.log(tgX);
-	});
+// 	$(window).on('mousemove', function(event) {
+// 		tgX = event.clientX;
+// 		tgY = event.clientY;
+// 		// console.log(tgX);
+// 	});
 
-	move();
-});
+// 	move();
+// });
