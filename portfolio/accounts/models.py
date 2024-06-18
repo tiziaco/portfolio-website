b27a7a3acc503f6ed85caa1833952ab1805453bd
tiziaco <tiziano.iaco@gmail.com>
2024-06-18 17:16:52 +0200

    increase length about field

--- portfolio/accounts/models.py
+++ portfolio/accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    about = models.TextField(max_length=500, blank=True)
+    about = models.TextField(max_length=1000, blank=True)
