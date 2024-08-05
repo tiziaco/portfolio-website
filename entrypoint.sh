3ec89e60cc1d02e730d69099d7ad87615a5cd255
tiziaco <tiziano.iaco@gmail.com>
2024-08-05 10:53:23 +0200

    update Docker setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,29 @@
 . $(poetry env info --path)/bin/activate
 
 # Run database migrations
+echo "\n*** Make database migrations..."
 python manage.py migrate --no-input
 
 # Collect static files
+echo "\n*** Collect static files..."
 python manage.py collectstatic --no-input
 
 
 # Create superuser if not exists
 if [ "$DJANGO_SUPERUSER_NAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] && [ "$DJANGO_SUPERUSER_EMAIL" ]; then
-    echo "*** Creating superuser..."
-    echo "*** super user: $DJANGO_SUPERUSER_NAME"
-    echo "*** super pass: $DJANGO_SUPERUSER_PASSWORD"
+    echo "\n*** Creating superuser..."
+    echo "super user: $DJANGO_SUPERUSER_NAME"
     # Ensure that the superuser creation command is correctly using environment variables
     python manage.py createsuperuser --username "$DJANGO_SUPERUSER_NAME" --email "$DJANGO_SUPERUSER_EMAIL" --noinput || true
 fi
 
-# Start Django development server for development
+# Start Django server
 if [ "$DJANGO_DEBUG" = "True" ]; then
-    echo "*** Running server..."
+    echo "\n*** Running server in Development mode..."
     exec python manage.py runserver 0.0.0.0:8000
 else
     # Start Gunicorn server for production
-    exec "$@"
+    echo "\n*** Running server in Deploy mode..."
+    gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
+    # exec "$@"
 fi
-# gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
