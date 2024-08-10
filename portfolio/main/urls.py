bdaed2d6c4c5a37d3a487482f77a1dc63741d667
tiziaco <tiziano.iaco@gmail.com>
2024-08-10 11:14:18 +0200

    fix landing page url

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
 	path("", views.home, name='home'),
-	path("home/", views.home, name='home'),
 	path('project/<slug:project_slug>/', views.project, name='project_detail'),
 	path("contact/", views.contact, name='contact'),
 ]
