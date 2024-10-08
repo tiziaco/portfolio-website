12a70ca613e7949bad51004ee99c0e86d88c42ad
tiziaco <tiziano.iaco@gmail.com>
2024-10-08 15:53:27 +0200

    change class name for styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({children,}: Readonly<{
 					enableSystem
 					disableTransitionOnChange
 				>
-					<div className="main">
+					<div className="background">
 						<Background />
 					</div>
 					<main className='app'>
