ede75f36dbb6031fafc1ffcbf61e9975368aa8df
tiziaco <tiziano.iaco@gmail.com>
2024-10-09 16:53:39 +0200

    update project content

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
 		id: 1,
 		title: "Minishell",
 		des: "A simplified Unix shell that replicates core features like executing commands, handling pipes, managing redirections, and returning accurate error codes. It offers hands-on experience with system calls, process management, error handling, and C programming",
-		img: "/p1.svg",
+		img: "/msh_0.png",
 		techStack: ["C", "UNIX"],
 		webLink: "",
 		gitLink: "https://github.com/tiziaco/minishell"
@@ -12,7 +12,7 @@ export const projects = [
 		id: 2,
 		title: "MiniRT",
 		des: "This project introduces the captivating world of ray tracing, a key technique in computer graphics that simulates light interactions to create realistic images. By generating detailed reflections, shadows, and textures, it brings virtual scenes to life with impressive precision.",
-		img: "/p2.svg",
+		img: "/minirt_0.png",
 		techStack: ["C", "UNIX", "X11"],
 		webLink: "",
 		gitLink: "www.github.com"
@@ -21,7 +21,7 @@ export const projects = [
 		id: 3,
 		title: "iTrader",
 		des: "Powerful algorithmic trading framework designed for both backtesting and live execution of trading strategies. Built on an event-driven architecture, it provides a flexible and modular environment for developing and deploying automated trading strategies.",
-		img: "/p3.svg",
+		img: "/itrader_0.png",
 		techStack: ["Python","Pandas", "Poetry", "SQL"],
 		webLink: "",
 		gitLink: "https://github.com/tiziaco/intelli-trader"
@@ -30,7 +30,7 @@ export const projects = [
 		id: 4,
 		title: "Chat PDF",
 		des: "Full-stack web application that mimics the conversational experience of ChatGPT. It leverages large language models (LLM) for core functionality and integrates a Retrieval-Augmented Generation (RAG) component to access and analyze PDF files.",
-		img: "/p4.svg",
+		img: "/llm_chat_0.png",
 		techStack: ["Python","LlamaIndex", "Ollama", "Streamlit", "PostgreSQL"],
 		webLink: "",
 		gitLink: "https://github.com/tiziaco/llm-rag-app"
