e6e7b748a62a2d0080268f3bd762b8a539af93a4
tiziaco <tiziano.iaco@gmail.com>
2024-10-10 11:36:26 +0200

    implement server action call for env variables

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -1,11 +1,19 @@
 'use client';
 
-import { sendContactForm } from '@/actions/contact';
-import { useState, useTransition, FormEvent } from 'react';
-import LitUpButton from './LitUpButton';
+import { useState, useEffect, useTransition, FormEvent } from 'react';
 import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import { sendContactForm } from '@/actions/sendEmail';
+import { getEnvVariables } from '@/actions/getPublicEnv';
+import LitUpButton from './LitUpButton';
 
-function ContactFormContent() {
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+interface SentEmailState {
+	setIsEmailSent: React.Dispatch<React.SetStateAction<boolean>>;
+  }
+
+function ContactFormContent({ setIsEmailSent } : SentEmailState) {
 	const [fullname, setFullname] = useState<string>('');
 	const [email, setEmail] = useState<string>('');
 	const [message, setMessage] = useState<string>('');
@@ -22,12 +30,13 @@ function ContactFormContent() {
 		}
 
 		const token = await executeRecaptcha('contact_form');
-		console.log('reCAPTCHA token:', token);
+		// console.log('reCAPTCHA token:', token);
 
 		startTransition(async () => {
 			const result = await sendContactForm({ fullname, email, message, token });
 			if (result.success) {
 				setStatus('Message sent successfully');
+				setIsEmailSent(true);
 				setFullname('');
 				setEmail('');
 				setMessage('');
@@ -73,15 +82,48 @@ function ContactFormContent() {
 
 				<LitUpButton isPending={isPending}/>
 			</form>
-			{status && <p>{status}</p>}
 		</div>
 	);
 }
 
 export default function ContactForm() {
+	const [reCaptchaKey, setReCaptchaKey] = useState<string>('')
+	const [isEmailSent, setIsEmailSent] = useState(false)
+
+	useEffect(() => {
+		async function fetchEnvVariables() {
+			const envVars = await getEnvVariables()
+			setReCaptchaKey(envVars.RECAPTCHA_SITE_KEY || 'error')
+		}
+		fetchEnvVariables();
+	}, [])
+
+	useEffect(() => {
+		if (isEmailSent) {
+		  toast.success('Email sent successfully!', {
+			position: "top-right",
+			autoClose: 3000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+		  })
+		  // Reset the state after showing the toast
+		  setIsEmailSent(false)
+		}
+	}, [isEmailSent])
+
 	return (
-		<GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''}>
-			<ContactFormContent />
-		</GoogleReCaptchaProvider>
-	);
+		<>
+		  {/* Only render the GoogleReCaptchaProvider if the reCaptchaKey is valid */}
+		  {reCaptchaKey && reCaptchaKey !== 'error' ? (
+			<GoogleReCaptchaProvider reCaptchaKey={reCaptchaKey}>
+			  <ContactFormContent setIsEmailSent={setIsEmailSent} />
+			</GoogleReCaptchaProvider>
+		  ) : (
+			<p>Loading reCAPTCHA...</p>
+		  )}
+		  <ToastContainer />
+		</>
+	  );
 }
