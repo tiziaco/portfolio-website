0d995f148c42ebee1d2cfd6fe0d4160bffdaea94
tiziaco <tiziano.iaco@gmail.com>
2024-10-14 19:08:01 +0200

    refacto bento grid item

--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -24,16 +24,7 @@ export const BentoGrid = ({
 	);
 };
 
-export const BentoGridItem = ({
-	className,
-	id,
-	title,
-	description,
-	img,
-	imgClassName,
-	titleClassName,
-	spareImg,
-}: {
+interface BentoGridItemProps {
 	className?: string;
 	id: number;
 	title?: string | React.ReactNode;
@@ -42,87 +33,85 @@ export const BentoGridItem = ({
 	imgClassName?: string;
 	titleClassName?: string;
 	spareImg?: string;
-}) => {
-	return (
-		<div
-			className={cn(
-				"relative overflow-hidden glassmorphism rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
-				className
+}
+
+export const BentoGridItem: React.FC<BentoGridItemProps> = ({
+	className,
+	id,
+	title,
+	description,
+	img,
+	imgClassName,
+	titleClassName,
+	spareImg,
+	}) => {
+	const renderImage = (imageSrc?: string, imgClass?: string, containerClass?: string) => (
+		<div className={cn(containerClass, 'relative w-full h-full')}>
+			{imageSrc && (
+			<div className="absolute inset-4">
+			<Image
+				src={imageSrc}
+				alt={imageSrc}
+				fill
+				className={cn(imgClass, 'rounded-2xl object-cover object-center')}
+				style={{ 
+				objectFit: id === 1 ? 'contain' : 'cover',
+				objectPosition: 'center'
+				}}
+				/>
+			</div>
 			)}
-			// style={{
-			// 	background: "rgb(4,7,29)",
-			// 	backgroundColor:
-			// 		"linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-			// }}
-		>
-			{/* Add image for Item 6 (bottom-right) */}
-			<div className={`${id === 6 && "flex justify-center"} h-full`}>
-				<div className="w-full h-full absolute p-5">
-					{img && (
-						<img
-							src={img}
-							alt={img}
-							className={cn(imgClassName, "object-cover object-center ")}
-						/>
-					)}
-				</div>
+		</div>
+		);
 
-				{/* Add Image for Item 5 */}
-				<div
-					className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-						} `}
-				>
-					{spareImg && (
-						<img
-							src={spareImg}
-							alt={spareImg}
-							className="object-cover object-center w-full h-full"
-						/>
-					)}
-				</div>
+	const renderTitleAndDescription = () => (
+		<div className={cn(titleClassName,
+		'z-10 flex flex-col p-5 lg:p-10 justify-center items-center',
+		'absolute inset-0')}>
+			<div className="font-sans text-lg lg:text-3xl font-bold">{title}</div>
+			{description && (
+			<div className="font-sans font-extralight max-w-48 md:text-xs lg:text-base text-sm text-[#C1C2D3]">
+				{description}
+			</div>
+			)}
+		</div>
+	);
 
-				{/* Title and Description Rendering */}
-				<div
-					className={cn(
-						titleClassName,
-						"group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 justify-center items-center"
-					)}
-				>
-					{/* Conditional rendering based on the ID */}
-					{ id === 4 ? (
-						// Layout for ID 4 (title on left, tech stack on right)
-						<div className="flex justify-between items-start relative">
-							{/* Left div for title and description */}
-							<div className="flex-1 justify-center ite">
-								<div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
-									{title}
-								</div>
-								<div className="font-sans font-light max-w-48 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
-									{description}
-								</div>
-							</div>
-
-							{/* Right div for the tech stack pills */}
-							<div className="flex-1 flex justify-end">
-								<PillBadges items={techStack} />
-							</div>
-						</div>
-					) : (
-						// Default layout for all other items
-						<>
-							{/* Render the description and the title of the card Item */}
-							<div className={`font-sans text-lg lg:text-3xl font-bold z-10`}>
-								{title}
-							</div>
-							{description && (
-								<div className="font-sans font-extralight max-w-48 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
-								{description}
-								</div>
-							)}
-						</>
-					)}
+	const renderLayoutForID4 = () => (
+		<div className="flex justify-between items-start p-5 lg:p-10 z-10">
+			<div className="flex-1">
+				<div className="font-sans text-lg lg:text-3xl max-w-96 font-bold">
+					{title}
 				</div>
+				<div className="font-sans font-light max-w-48 md:text-xs lg:text-base text-sm text-[#C1C2D3]">
+					{description}
+				</div>
+			</div>
+			<div className="flex-1 flex justify-end">
+				{techStack && <PillBadges items={techStack} />}
 			</div>
 		</div>
 	);
-};
+  
+	return (
+		<div
+		className={cn(
+			'relative overflow-hidden glassmorphism rounded-3xl border border-white/[0.1] shadow-xl dark:shadow-none',
+			'min-h-[200px]',
+			className
+		)}
+		>
+			<div className="absolute inset-0">
+				{renderImage(img, imgClassName)}
+			</div>
+			{spareImg && (
+				<div className={cn('absolute right-0 bottom-0', id === 5 ? 'w-full h-full opacity-80' : 'w-1/2 h-1/2')}>
+					{renderImage(spareImg, 'object-cover object-center')}
+				</div>
+			)}
+			<div className="relative h-full flex flex-col justify-between items-center">
+				{id === 4 ? renderLayoutForID4() : renderTitleAndDescription()}
+			</div>
+		</div>
+	);
+  };
