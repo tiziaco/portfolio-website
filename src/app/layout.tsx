222895682ce04c33a3f82c309d233b4cc1a6af66
tiziaco <tiziano.iaco@gmail.com>
2024-10-23 17:47:12 +0200

    optimize metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,54 @@ import GoogleAnalytics from "@/components/GoogleAnalytics";
 const inter = Inter({subsets: ["latin"]})
 
 export const metadata: Metadata = {
-	title: "Tiziano's portfolio",
-	description: "Tiziano's developer portfolio website.",
+	title: "Tiziano | Full-Stack Developer & Software Engineer Portfolio",
+	description: "Explore Tiziano's portfolio: Full-Stack developer skilled in modern technologies like Next.js, JavaScript, Python and Docker. Explore projects, case studies, and experience in building scalable web applications and software solutions.",
+	keywords: ["Software Engineer", "Full-Stack Developer", "DevOps", "AI", "JavaScript", "Python", "Next.js", "Docker", "Web Development", "Portfolio"],
+	authors: [{ name: "Tiziano Iacovelli", url: "https://www.developertiz.com" }],
+	creator: "Tiziano Iacovelli",
+	publisher: "Tiziano I.",
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: false,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	alternates: {
+		canonical: "https://developertiz.com",
+	},
+	openGraph: {
+		title: "Tiziano | Full-Stack Developer Portfolio",
+		description: "Explore Tiziano's projects and skills in web development and software engineering",
+		url: "https://developertiz.com",
+		siteName: "Tiziano's Portfolio",
+		images: [
+		{
+			url: "https://developertiz.com/og_image.png",
+			width: 1200,
+			height: 630,
+			alt: "Tiziano's Portfolio",
+		},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Tiziano | Software Developer Portfolio",
+		description: "Explore Tiziano's projects and skills in software engineering and web development",
+		creator: "@Tiz_iaco",
+		images: ["https://developertiz.com/og_image.png"],
+		},
 	icons: {
 		icon: '/favicon.ico',
-		},
+	},
 };
 
 export default function RootLayout({children,}: Readonly<{
