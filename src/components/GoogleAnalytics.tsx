329c6cd8c895d7040f96eea472f4df00366954d9
tiziaco <tiziano.iaco@gmail.com>
2024-10-30 15:18:18 +0100

    refactor GA component

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,25 +1,40 @@
+"use client"
+
+import { useEffect } from 'react';
 import Script from 'next/script';
 
-const GoogleAnalytics = () => {
-  return (
-    <>
-      <Script
-        strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      />
+type WindowWithDataLayer = Window & {
+	dataLayer: Record<string, any>[];
+};
 
-      <Script id='' strategy='lazyOnload'>
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
-      </Script>
-    </>
-  );
+declare const window: WindowWithDataLayer;
+
+const GoogleAnalytics: React.FC = () => {
+	useEffect(() => {
+		window.dataLayer = window.dataLayer || [];
+		function gtag(...args: any[]) {
+			window.dataLayer.push(args);
+		}
+		gtag('js', new Date());
+		gtag('config', process.env.NEXT_PUBLIC_MEASUREMENT_ID);
+	}, []);
+
+	return (
+		<>
+			<Script
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+				`}
+			</Script>
+		</>
+	);
 };
 
 export default GoogleAnalytics;
