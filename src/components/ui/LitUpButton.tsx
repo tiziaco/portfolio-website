823163df2aad404f0662771ae5feee2c9f48bca4
tiziaco <tiziano.iaco@gmail.com>
2024-10-31 18:39:08 +0100

    fix bug button

--- src/components/ui/LitUpButton.tsx
+++ src/components/ui/LitUpButton.tsx
@@ -1,18 +1,19 @@
 
-const LitUpButton = ({isPending} : 
-	{isPending:boolean}) => {
-  return (
-	<div className="flex justify-end">
-		<button
-			type="submit"
-			className="p-[3px] relative">
-			<div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-			<div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-			{isPending ? 'Sending...' : 'Send Message'}
-			</div>
-		</button>
-	</div>
-  )
+const LitUpButton = ({ isPending }: {isPending:boolean}) => {
+	return (
+		<div className="flex justify-end">
+			<button
+				type="submit"
+				className={`p-[3px] relative ${isPending ? 'opacity-50 cursor-not-allowed' : ''}`}
+				disabled={isPending}
+			>
+				<div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+				<div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+					{isPending ? 'Sending...' : 'Send Message'}
+				</div>
+			</button>
+		</div>
+	);
 }
 
 export default LitUpButton
