92d8363169af66ac082805ead46b5b352dfa916e
tiziaco <tiziano.iaco@gmail.com>
2025-03-22 08:31:45 +0100

    feat: wrap ContactForm in ReCaptchaProvider for enhanced security

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,19 @@
-import ContactForm from "./ui/ContactForm"
+import { ContactForm } from "./ui/ContactForm"
+import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 const Contact = () => {
 	return (
 		<section id='contact'>
-			<div className="py-20">
-				{/* Section title */}
-				<h2 className="heading drop-shadow-2xl">
-					Found something interesting? {' '}
-					<span className="text-green-500 drop-shadow-3xl">Contact Me</span>
-				</h2>
-				<ContactForm />
-			</div>
+			<ReCaptchaProvider >
+				<div className="py-20">
+					{/* Section title */}
+					<h2 className="heading drop-shadow-2xl">
+						Found something interesting? {' '}
+						<span className="text-green-500 drop-shadow-3xl">Contact Me</span>
+					</h2>
+					<ContactForm />
+				</div>
+			</ReCaptchaProvider>
 		</section>
 	)
 }
