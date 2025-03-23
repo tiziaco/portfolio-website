d456c9dbae7c1a8f29368b6ab11e3335516877d0
tiziaco <tiziano.iaco@gmail.com>
2025-03-23 11:34:09 +0100

    feat: add SVG support in webpack configuration using SVGR

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      include: /public\/.*\.svg$/, // this allows icon.svg in app dir to work. All SVGs to be handled by SVGR are in src/assets
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {
