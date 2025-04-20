a881c3787662198d0fb26a16b50d9f64265f4aec
tiziaco <tiziano.iaco@gmail.com>
2025-04-20 11:01:24 +0200

    fix: update footer background color and remove unnecessary text color class

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,19 +12,19 @@ export const Footer = () => {
 	const currentYear = date.getFullYear();
 
 	return (
-	<footer className="bg-[#13162D]">
+	<footer className="bg-card">
 		<div className="mx-auto md:px-10 lg:px-20 py-6">
 			<div className="flex flex-col w-full md:flex-row items-center justify-between">
 
 				{/* Logo */}
 				<div className="relative">
 					<Link href='/' aria-label="link to hero section" className='flex gap-2 flex-center relative w-full h-8'>
-						<Logo className="w-64 h-auto text-white" />
+						<Logo className="w-64 h-auto" />
 					</Link>
 				</div>
 
 				{/* Copyright Text */}
-				<div className="text-center md:text-right dark:text-white">
+				<div className="text-center md:text-right">
 					<p>&copy;{currentYear}. Crafted with care</p>
 				</div>
 
